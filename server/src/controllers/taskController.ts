0ce534574961fdf74e6b2341acba823d5ba20307
o6b7 <alarkie2210@gmail.com>
2025-10-15 06:14:23 +0300

    Added get user tasks in task controller

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -57,3 +57,27 @@ export const updateTaskStatus = async (req: Request, res: Response): Promise<voi
         res.status(500).json({ message: "Error updating task: " + error.message })
     }
 }
+
+export const getUserTasks = async (req: Request, res: Response) : Promise<void> => {
+    const { userId } = req.params;
+
+    try {
+        const tasks = await prisma.task.findMany({
+            where: {
+                OR: [
+                    { authorUserId: Number(userId) },
+                    { assignedUserId: Number(userId) },
+                ]
+            },
+            include: {
+                author: true,
+                assignee: true,
+            }
+        })
+
+        res.json(tasks);
+
+    } catch (error: any) {
+        res.status(500).json({ message: "Error fetching user tasks: " + error.message })
+    }
+}
